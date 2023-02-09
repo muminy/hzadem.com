338f8fe3f535baf29c91692f763f86cf8bb5c0b2
Mümin Yıldırım <mmnyldrm61@gmail.com>
2023-02-09 22:01:24 +0300

    error fixs

--- helpers/errorMessage.ts
+++
@@ -1,9 +0,0 @@
-import errorStatus from "@/constants/errorStatus"
-import { ErrorCommonType } from "../types"
-import { getErrorStatus } from "./handleError"
-
-export default function getError({ error }: ErrorCommonType) {
-  const code = getErrorStatus(error) ?? 100000
-
-  return { code, message: errorStatus[code] }
-}
